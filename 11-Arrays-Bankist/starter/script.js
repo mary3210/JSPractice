54bbdd0f29fa1f67f6aa6044d60dff0660e154c7
Mary Rodriguez <maryrodri3210@gmail.com>
2024-04-15 11:55:19 -0500

    .at method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -75,39 +75,52 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+// let arr = ['a', 'b', 'c', 'd', 'e'];
 
-//Slice - Makes a shallow COPY of the original array
-console.log(arr.slice(2));
+// //Slice - Makes a shallow COPY of the original array
+// console.log(arr.slice(2));
 
-console.log(arr.slice(2, 4));
+// console.log(arr.slice(2, 4));
 
-console.log(arr.slice(-2));
+// console.log(arr.slice(-2));
 
-console.log(arr.slice(-1));
+// console.log(arr.slice(-1));
 
-console.log(arr.slice(1, -2));
+// console.log(arr.slice(1, -2));
 
-console.log(arr.slice());
+// console.log(arr.slice());
 
-//splice - Changes the original array
+// //splice - Changes the original array
 
-// console.log(arr.splice(2));
-console.log(arr.splice(-1));
-arr.splice(1,2);
-console.log(arr)
+// // console.log(arr.splice(2));
+// console.log(arr.splice(-1));
+// arr.splice(1,2);
+// console.log(arr)
 
-//Reverse
-arr = ['a', 'b', 'c', 'd', 'e'];
+// //Reverse
+// arr = ['a', 'b', 'c', 'd', 'e'];
 
-const arr2 = ['j', 'i', 'h', 'g', 'f']
+// const arr2 = ['j', 'i', 'h', 'g', 'f']
 
-console.log(arr2.reverse())
+// console.log(arr2.reverse())
 
-//Concat 
-const letters = arr.concat(arr2)
-console.log(letters);
-console.log([...arr, ...arr2])
+// //Concat 
+// const letters = arr.concat(arr2)
+// console.log(letters);
+// console.log([...arr, ...arr2])
 
-//Join
-console.log(letters.join(' - '))
+// //Join
+// console.log(letters.join(' - '))
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+
+//getting last array element
+console.log(arr[arr.length - 1])
+console.log(arr.slice(-1)[0])
+console.log(arr.at(-1));
+
+console.log('jonas'.at(0))
+console.log('jonas'.at(-1))
