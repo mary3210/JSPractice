a9e1525e1ed8cf8da13d4dfc2b4f02a08a66a2da
Mary Rodriguez <maryrodri3210@gmail.com>
2024-04-23 18:19:39 -0500

    the Remainder operator

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -281,7 +281,7 @@ console.log(Number.isNaN(23 / 0));
 console.log(Number.isFinite(20))
 console.log(Number.isFinite('20'))
 console.log(Number.isFinite(+'20X'));
-console.log(Number.isFinite( 23 / 0 ));
+console.log(Number.isFinite(23 / 0));
 
 console.log(Number.isInteger(23))
 
@@ -289,8 +289,8 @@ console.log(Number.isInteger(23))
 /////////////////////////////////////////////////////
 
 console.log(Math.sqrt(25))
-console.log(25 ** (1/2));
-console.log(8 ** (1/3));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
 
 console.log(Math.max(5, 18, 23, 11, 2));
 
@@ -300,9 +300,9 @@ console.log(Math.min(5, 18, 23, 11, 2));
 
 console.log(Math.PI * Number.parseFloat('10px') ** 2);
 //Math.trunc not versatile when it comes to negs but math.floor is. 
-console.log(Math.floor(Math.random() * 6) +1);
+console.log(Math.floor(Math.random() * 6) + 1);
 
-const randomInt = (min, max) => Math.trunc(Math.random() * (min - max) +1) + min
+const randomInt = (min, max) => Math.trunc(Math.random() * (min - max) + 1) + min
 
 console.log(randomInt(10, 20))
 
@@ -312,3 +312,26 @@ console.log(+(2.7).toFixed(3));
 console.log(+(2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
 
+//////////////////////////////////////////////////////////
+
+console.log(5 % 2);
+console.log(5 / 2);
+
+console.log(8 % 3)
+console.log(8 / 3)
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514))
+
+console.log(labelBalance)
+
+labelBalance.addEventListener('click', function(){
+  [...document.querySelectorAll('.movements__row')].
+  forEach(function(row, i){
+    if ( i % 2 === 0) row.style.backgroundColor = 'orange';
+  
+    if ( i % 3 === 0) row.style.backgroundColor = 'blue';
+  });   
+});
