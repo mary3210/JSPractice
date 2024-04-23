f31df21da32065d7d22c43bae525d27773ccc62c
Mary Rodriguez <maryrodri3210@gmail.com>
2024-04-23 16:30:14 -0500

    Math and Rounding

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -93,7 +93,7 @@ const displayMovements = function (movements, sort = false) {
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i + 1
       } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -103,19 +103,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -125,7 +125,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -204,7 +204,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -285,4 +285,30 @@ console.log(Number.isFinite( 23 / 0 ));
 
 console.log(Number.isInteger(23))
 
-// console.log(Math.sqrt(25))
+
+/////////////////////////////////////////////////////
+
+console.log(Math.sqrt(25))
+console.log(25 ** (1/2));
+console.log(8 ** (1/3));
+
+console.log(Math.max(5, 18, 23, 11, 2));
+
+console.log(Math.max(5, 18, "23", 11, 2));
+
+console.log(Math.min(5, 18, 23, 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+//Math.trunc not versatile when it comes to negs but math.floor is. 
+console.log(Math.floor(Math.random() * 6) +1);
+
+const randomInt = (min, max) => Math.trunc(Math.random() * (min - max) +1) + min
+
+console.log(randomInt(10, 20))
+
+//Rounding decimals
+console.log(+(2.7).toFixed(0));
+console.log(+(2.7).toFixed(3));
+console.log(+(2.345).toFixed(2));
+console.log(+(2.345).toFixed(2));
+
