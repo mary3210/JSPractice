b575be31a1e1a6dccf9365b8cafe5dbfca0f7dda
Mary Rodriguez <maryrodri3210@gmail.com>
2024-04-23 18:51:40 -0500

    Numeric Operators

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -335,3 +335,23 @@ labelBalance.addEventListener('click', function(){
     if ( i % 3 === 0) row.style.backgroundColor = 'blue';
   });   
 });
+
+/////////////////////////////////////////////////////////////
+
+
+//287,460,000,000
+const diameter = 287_460_000_000
+console.log(diameter);
+
+const priceCents = 345_99;
+console.log(priceCents);
+
+const transferFee1 = 15_00;
+console.log(transferFee1)
+const transferFee2 = 1_5_00;
+console.log(transferFee2)
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('230000'));
