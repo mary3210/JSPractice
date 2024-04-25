d0831b5c6aa50bf30384aee5587652246c197aa7
Mary Rodriguez <maryrodri3210@gmail.com>
2024-04-25 16:11:17 -0500

    Working with BigInt

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -371,4 +371,46 @@ console.log(BigInt(42134124312432434532543254353453534n))
 //Operations
 console.log(1000n + 1000n)
 
-console.log(d)
+
+
+////////////////////////////////////////////////////////////////////
+
+
+//Create a date
+// const now = new Date();
+// console.log(now)
+
+// console.log(new Date('Aug 02 2020 10:05:41'))
+
+// console.log(account1)
+// console.log(new Date(account1.movementsDates[0]));
+
+
+// console.log(new Date(2037, 10, 19, 15, 23, 5));
+
+// console.log(new Date(0));
+// //3 days which has 24 hours each day, with each hour is 60 minutes, with each min is made out of 60 seconds, and times 1000 to convert to miliseconds
+// console.log(new Date(3 * 24 * 60 * 60 * 1000))
+
+//Working with Dates
+const future = new Date(2037, 10, 19, 15, 23)
+console.log(future)
+console.log(future.getFullYear());
+console.log(future.getMonth());
+//day of the month
+console.log(future.getDate());
+//the day of the week 
+console.log(future.getDay());
+
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142256980000));
+
+console.log(Date.now())
+
+future.setFullYear(2040);
+console.log(future)
