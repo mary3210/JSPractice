ad6845b860763cf5cd3e4453539239803b39c142
Mary Rodriguez <maryrodri3210@gmail.com>
2024-04-30 12:54:03 -0500

    Dom Traversing

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -142,15 +142,15 @@ btnScrollTo.addEventListener('click', function(e){
   // section1.scrollIntoView({ behavior: 'smooth'})
 })
 
-const h1 = document.querySelector('h1')
+// const h1 = document.querySelector('h1')
 
-h1.addEventListener('mouseenter', function(e){
-    alert('addEventListener: Great you are reading the heading :D')
-})
+// h1.addEventListener('mouseenter', function(e){
+//     alert('addEventListener: Great you are reading the heading :D')
+// })
 
-h1.onmouseenter = function(e) {
-    alert('onmouseenter: Great! You are reading the heading :D')
-}
+// h1.onmouseenter = function(e) {
+//     alert('onmouseenter: Great! You are reading the heading :D')
+// }
 
 
 
@@ -209,3 +209,34 @@ document.querySelector('.nav__links').addEventListener
         })
     }
 })
+
+
+
+///////////////////////////////////////////////////////////////
+const h1 = document.querySelector('h1');
+
+//Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+//Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary';
+
+//Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.parentElement.children);
+
+[...h1.parentElement.children].forEach(function(el) {
+    if (el !== h1) el.style.transform = 'scale(0.5)';
+});
+
