0aa5c23aa693112625284bdae3744717c4722fe5
Mary Rodriguez <maryrodri3210@gmail.com>
2024-04-30 10:16:43 -0500

    event handlers

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault()
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,8 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +30,126 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//selecting elements
+console.log(document.documentElement)
+console.log(document.head)
+console.log(document.body)
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section')
+console.log(allSections)
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+document.getElementsByClassName('btn');
+
+// Creating and inserting elements
+
+
+const message = document.createElement('div')
+message.classList.add('cookie-message');
+
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class ="btn btn--close-cookie">Got it!</button>';
+
+
+//first child of the element
+// header.prepend(message);
+//last child of the element
+header.append(message);
+// header.append(message.cloneNode(true));
+
+header.before(message);
+header.after(message);
+
+//Delete elements
+document.querySelector('.btn--close-cookie').
+  addEventListener('click', function () {
+    // message.remove();
+    message.parentElement.removeChild(message);
+  })
+
+
+//style
+message.style.backgroundColor = '#37383d'
+message.style.width = '120%';
+
+console.log(message.style.height)
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+
+console.log(message.style.height)
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+//attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.src)
+console.log(logo.alt)
+console.log(logo.className)
+
+logo.alt = 'Beautiful minimalist logo';
+
+
+//Non-standard
+// console.log(logo.designer)
+logo.setAttribute('company', 'Bankist')
+
+//absolute pathing
+console.log(logo.src)
+//Relative pathing
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+//Classes 
+logo.classList.add('c','j');
+logo.classList.remove('c','j');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+//Don't use
+logo.className = 'jonas'
+
+/////////////////////////////////////////////////////////
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+console.log(btnScrollTo)
+btnScrollTo.addEventListener('click', function(e){
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  // console.log(e.target.getBoundingClientRect());
+
+  // console.log('Current scroll (X/Y', window.scrollX, window.scrollY)
+
+  // console.log(
+  //   'height/width viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // )
+  
+  // section1.scrollIntoView({ behavior: 'smooth'})
+})
+
+const h1 = document.querySelector('h1')
+
+h1.addEventListener('mouseenter', function(e){
+  alert('addEventListener: Great you are reading the heading :D')
+})
+
+h1.onmouseenter = function(e) {
+  alert('onmouseenter: Great! You are reading the heading :D')
+}
+
+setTimeout(() => h1.remove.EventListener('mouseenter', alertH1), 3000)
