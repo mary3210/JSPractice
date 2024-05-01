18f99ac04c8dcefb03fee940a41f8bce17aa8f70
Mary Rodriguez <maryrodri3210@gmail.com>
2024-05-01 11:47:01 -0500

    Passing Arguements to Event Handlers

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -240,3 +240,29 @@ console.log(h1.parentElement.children);
     if (el !== h1) el.style.transform = 'scale(0.5)';
 });
 
+///////////////////////////////////////////////////////////////////////////
+// Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function(e){
+    const clicked = e.target.closest('.operations__tab');
+    console.log(clicked)
+
+//Guard clause
+if(!clicked) return;
+
+
+//active tabs
+tabs.forEach(t => t.classList.remove('operations__tab--active'));
+clicked.classList.add('operations__tab--active');
+
+console.log(clicked)
+console.log(clicked.dataset)
+console.log(clicked.dataset.potato)
+//activate content area
+document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add();
+
+});
+
