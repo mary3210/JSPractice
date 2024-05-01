d7b2f9574738ff45b21bde29be3d69414c979cdf
Mary Rodriguez <maryrodri3210@gmail.com>
2024-05-01 18:05:05 -0500

    the intersection api

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -299,13 +299,51 @@ nav.addEventListener('mouseout', function(e){
 
 //Sticky Navigation
 const initialCoords = section1.getBoundingClientRect()
-console.log(initialCoords)
 
 window.addEventListener('scroll', function(e){
-  console.log(window.scrollY)
 
-  if(this.window.scrollY > initialCoords.top) nav.classList.add('sticky')else{
+
+  if(this.window.scrollY > initialCoords.top) {nav.classList.add('sticky')}
+  else{
     nav.classList.remove('sticky');
   }
 
 })
+
+//////////////////////////////////////////////////
+
+// const obsCallback = function (entries, observer){
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   })
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: 0.1
+// }
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1)
+
+const header1 = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight)
+console.log(header1)
+const stickyNav = function(entries){
+  const [entry] = entries;
+  console.log(entry)
+
+  if(!entry.isIntersecting)
+  nav.classList.add('sticky')
+  else nav.classList.remove('sticky')
+}
+
+const headerObserver = new IntersectionObserver(
+  stickyNav,{
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`
+  }
+);
+headerObserver.observe(header1);
